56825b09eabd505e0102ee62eae389397b17fe93
unknown <ashwinmurali27@gmail.com>
2023-04-15 12:25:40 +0530

    minor changes 2

--- installer.bat
+++ installer.bat
@@ -6,7 +6,7 @@ echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\Cleaner.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "%cd%\temp_cleaner.bat" >> %SCRIPT%
-echo oLink.IconLocation = "%cd%\icon2.ico" >>%SCRIPT%
+echo oLink.IconLocation = "%cd%\icon.ico" >>%SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
